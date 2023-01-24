b0618002e0109c4a6a49243c1f993da1b1175d69
ive-m <ivesita.maria@gmail.com>
2023-01-24 14:59:20 -0500

    Add Get Reservations by ID

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -74,26 +74,39 @@ const addUser = function (user) {
   });
 };
 //getAllProperties();
+
+
+
+
+
+
+ const getAllReservations = function(guest_id, limit = 10) {
+  return pool
+  .query(
+    `SELECT reservations.*, properties.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $2
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $1;`,
+    [10,guest_id])
+  .then((result) => {
+    return result.rows;
+   // console.log(result.rows);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+};
+
 exports.getUserWithEmail = getUserWithEmail;
 exports.getAllProperties = getAllProperties;
 exports.getUserWithId = getUserWithId;
 exports.addUser = addUser;
-
-
-
-
-
-// /// Reservations
-
-// /**
-//  * Get all reservations for a single user.
-//  * @param {string} guest_id The id of the user.
-//  * @return {Promise<[{}]>} A promise to the reservations.
-//  */
-// const getAllReservations = function(guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// }
-// exports.getAllReservations = getAllReservations;
+exports.getAllReservations = getAllReservations;
+ 
 
 // /// Properties
 
