09710d3c0c52d3c9841c1f2b1307e339b17c091d
ive-m <ivesita.maria@gmail.com>
2023-01-26 14:09:09 -0500

    Modify README

--- LightBnB_WebApp-master/README.md
+++ LightBnB_WebApp-master/README.md
@@ -1,5 +1,15 @@
 # LightBnB
 
+The purpose of this project is to design a database and use server-side JavaScript to display the information from queries to web pages. I will be able to apply my existing knowledge of complex SQL queries, database and ERD (entity relationship diagram) design to integrate the database with a Node backend.
+
+In this project, I have:
+
+* Designed the database and create an ERD for the tables.
+* Created the database and the tables using the ERD.
+* Added fake data to the database to make testing queries easier.
+* Wrote queries.
+* Connected the database to a JavaScript application in order to interact with the data from a web page.
+
 ## Project Structure
 
 ```
@@ -40,4 +50,40 @@
   * `server.js` is the entry point to the application. This connects the routes to the database.
   * `apiRoutes.js` and `userRoutes.js` are responsible for any HTTP requests to `/users/something` or `/api/something`. 
   * `json` is a directory that contains a bunch of dummy data in `.json` files.
-  * `database.js` is responsible for all queries to the database. It doesn't currently connect to any database, all it does is return data from `.json` files.
+  * `database.js` is responsible for all queries to the database. 
+
+  ## Project Functionalities
+
+  * Search => the users search listed properties by city, price or rate.
+  * Login => log in if the user has an account.
+  * Sign Up => the user creates an account.
+  * Create Listing => users list a property for rent.
+  * My Listings => show all the listed properties owned by the user.
+  * My Reservations => show all reservations that the user has. 
+ 
+
+## Learning Outcomes
+In completing this project, I am able to:
+
+* Explain the importance and application of SQL/relational databases for web applications.
+* Describe the key components that make up a relational database.
+* Design database tables using primary and foreign keys correctly.
+* Determine if a data model should use one-to-many or many-to-many relationships when designing relational databases.
+* Utilize SELECT statements to solve common data query questions involving GROUP BY, WHERE, LIMIT, ORDER.
+* Utilize CREATE, UPDATE, DELETE statements to modify existing database table structure and records.
+* Use JOIN statements to combine information from related tables to build more complex result sets.
+* Use asynchronous (promise-based) postgres JS libraries to query data from Node applications.
+* Use the psql CLI to interact with and explore a database.
+* Research database queries through documentation and other references.
+
+## Work Outcomes
+By completing this project, I am being prepared to be able to do the following on the job:
+
+* Troubleshoot and debug problems associated with web development.
+* Demonstrate problem solving skills by developing basic algorithms to solve coding challenges or katas.
+* Evaluate or analyze coding solutions / ideas, and their limitations and/or benefits.
+* Contribute to full-stack web applications using modern software development patterns, tools and workflows.
+* Contribute to the backend of web applications that use industry-standard languages (Ruby) and frameworks (Rails and Express).
+* Integrate into common development team workflows (git, github workflow, automated testing, and code reviews).
+* Extend existing database design, as well as writing realistic SQL queries to CRUD data in SQL databases.
+* Create API services and/or consumers that use modern data formats such as JSON.
--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,3 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -10,7 +7,7 @@ const pool = new Pool({
   database: 'lightbnb'
 });
 
-//pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {});
+
 
 const getAllProperties = (options, limit = 10) => {
 
@@ -18,40 +15,41 @@ const getAllProperties = (options, limit = 10) => {
   let queryString = `SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
   JOIN property_reviews ON properties.id = property_id `;
-console.log('OPTIONS',options);
+
   if (options.owner_id) {
     queryParams.push(`${options.owner_id}`);
-        queryString += `WHERE owner_id = $${queryParams.length} `;}
+    queryString += `WHERE owner_id = $${queryParams.length} `;
+  }
   
   if (options.city) {
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city LIKE $${queryParams.length} `;}
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
 
 
   if (options.minimum_price_per_night && options.maximum_price_per_night) {
-        queryParams.push(`${options.minimum_price_per_night}`);
+    queryParams.push(`${options.minimum_price_per_night}`);
         
-        queryString += `AND $${queryParams.length}<= cost_per_night `;
+    queryString += `AND $${queryParams.length}<= cost_per_night `;
 
-        queryParams.push(`${options.maximum_price_per_night}`);
-        queryString +=`AND
+    queryParams.push(`${options.maximum_price_per_night}`);
+    queryString += `AND
         $${queryParams.length}>= cost_per_night  `;
       
     
-    }
+  }
 
   if (options.minimum_rating) {
-          queryParams.push(`${options.minimum_rating}`);
-          queryString += `AND property_reviews.rating>=$${queryParams.length}`;}
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `AND property_reviews.rating>=$${queryParams.length}`;
+  }
 
-    queryParams.push(limit);
-    queryString += `
+  queryParams.push(limit);
+  queryString += `
     GROUP BY properties.id
     ORDER BY cost_per_night
     LIMIT $${queryParams.length};
     `;
-    console.log('Query',queryString )
-    console.log('Parametros', queryParams)
     
 
   return pool
@@ -67,52 +65,51 @@ console.log('OPTIONS',options);
 };
 
  
-const getUserWithEmail = function (email) {
+const getUserWithEmail = function(email) {
 
   return pool
-  .query(
-    `SELECT * FROM users WHERE email= $1;`,
-    [email])
-  .then((result) => {
-    return result.rows[0];
-    //console.log(result.rows);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .query(
+      `SELECT * FROM users WHERE email= $1;`,
+      [email])
+    .then((result) => {
+      return result.rows[0];
+   
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
   
  
   
-const getUserWithId = function (id) {
+const getUserWithId = function(id) {
   return pool
-  .query(
-    `SELECT * FROM users WHERE id= $1;`,
-    [id])
-  .then((result) => {
-    //console.log(result.rows);
-     return result.rows[0];
+    .query(
+      `SELECT * FROM users WHERE id= $1;`,
+      [id])
+    .then((result) => {
+ 
+      return result.rows[0];
    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 
-const addUser = function (user) {
+const addUser = function(user) {
   return pool
-  .query(
-    `INSERT INTO users (name, email, password) VALUES($1,$2,$3) RETURNING *;`,
-    [user.name, user.email, user.password])
-  .then((result) => {
-    console.log('Object with id',result.rows);
-    return result.rows;
+    .query(
+      `INSERT INTO users (name, email, password) VALUES($1,$2,$3) RETURNING *;`,
+      [user.name, user.email, user.password])
+    .then((result) => {
+      return result.rows;
    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 
@@ -121,10 +118,10 @@ const addUser = function (user) {
 
 
 
- const getAllReservations = function(guest_id, limit = 10) {
+const getAllReservations = function(guest_id, limit = 10) {
   return pool
-  .query(
-    `SELECT reservations.*, properties.*, avg(rating) as average_rating
+    .query(
+      `SELECT reservations.*, properties.*, avg(rating) as average_rating
     FROM reservations
     JOIN properties ON reservations.property_id = properties.id
     JOIN property_reviews ON properties.id = property_reviews.property_id
@@ -132,42 +129,42 @@ const addUser = function (user) {
     GROUP BY properties.id, reservations.id
     ORDER BY reservations.start_date
     LIMIT $1;`,
-    [limit,guest_id])
-  .then((result) => {
-    return result.rows;
-   // console.log(result.rows);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+      [limit,guest_id])
+    .then((result) => {
+      return result.rows;
+      
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
-const addProperty = function (property) {
+const addProperty = function(property) {
   return pool
-  .query(
-    `INSERT INTO properties (title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,street,city,province,post_code,country,parking_spaces,number_of_bathrooms,number_of_bedrooms) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) RETURNING *;`,
-    [
-      property.title,
-      property.description,
-      property.thumbnail_photo_url,
-      property.cover_photo_url,
-      property.cost_per_night,
-      property.street,
-      property.city,
-      property.province,
-      property.post_code,
-      property.country,
-      property.parking_spaces,
-      property.number_of_bathrooms,
-      property.number_of_bedrooms])
-  .then((result) => {
+    .query(
+      `INSERT INTO properties (title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,street,city,province,post_code,country,parking_spaces,number_of_bathrooms,number_of_bedrooms) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) RETURNING *;`,
+      [
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms])
+    .then((result) => {
  
-    return result.rows;
+      return result.rows;
    
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 exports.getUserWithEmail = getUserWithEmail;
@@ -178,16 +175,3 @@ exports.getAllReservations = getAllReservations;
 exports.addProperty = addProperty;
 
 
-
-// /**
-//  * Add a property to the database
-//  * @param {{}} property An object containing all of the property details.
-//  * @return {Promise<{}>} A promise to the property.
-//  */
-// const addProperty = function(property) {
-//   const propertyId = Object.keys(properties).length + 1;
-//   property.id = propertyId;
-//   properties[propertyId] = property;
-//   return Promise.resolve(property);
-// }
-// exports.addProperty = addProperty;
